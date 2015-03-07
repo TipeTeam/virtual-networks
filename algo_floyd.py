7e801a185088930c8956c3bbed0b56e91cc20bf4
Rannios <rannios@terredefeu.fr>
2015-03-07 18:46:14 +0100

    Update algo_floyd.py

--- algo_floyd.py
+++ algo_floyd.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python3
-# coding: iso-8859-1 
+# coding: iso-8859-1
+
+# Algorithme de Floyd-Warshall, en O(n^3), permet de trouver tous les plus courts chemins dans un graphe
+# Rq: Dijkstra ne trouvait que les plus courts chemins issus d'UNE seule origine.
+ 
+import os
+inf = float("inf")
 
 def floyd(g):
-  w = []
-  #return w
   n = len(g)
+  w = []
   for i in range (n):
     w.append([])
     for j in range (n):
-      bande = g[i][j]
-      if bande == 0: bande = 4000000
-      elif bande < 0: bande = 0
-      else: bande = 1/bande
+      distance = g[i][j]
       w[i].append({
-        "bande": bande,  # bande passante : 0 si pas de liaison, grand si cool, -1 si osef
+        "distance": distance,  # distance : inf si pas de liaison, +petit c'est meilleur, 0 si osef
         "direction": j # voisin par lequel passer. Au début, on suppose qu'on peut aller direct de i à j 
       })
-  #return w
-  for k in range (n): # points atteints en k étapes (n au max car on passe pas 2fois au même endroit)
+
+  for k in range (n): # points atteints en k étapes ou moins (k = n au max car on passe pas 2fois au même endroit)
     for i in range (n): # traitement de chaque ordi comme point de départ
-      for j in range (n): # test de tous les voisins
+      for j in range (n): # test de toutes les destinations
         if i != j: # on va pas chercher la merde en demandant si c'est rapide d'aller de Paris à Paris...
-          bande_testee = w[i][k]["bande"] + w[k][j]["bande"]
-          if (bande_testee < w[i][j]["bande"]):
+          distance_testee = w[i][k]["distance"] + w[k][j]["distance"]
+          if (distance_testee < w[i][j]["distance"]):
             w[i][j] = {
-              "bande": bande_testee,
+              "distance": distance_testee,
               "direction": w[i][k]["direction"]
             }
+  for i in range (n):
+    for j in range (i+1,n):
+      print("Trajet de "+str(i)+" à "+str(j)+" : "+str(w[i][j]["distance"])+"km.\nDirigez-vous vers "+str(w[i][j]["direction"])+".\n\n")
   return w
 
-print(floyd(
+floyd(
 [
-  [-1, 1, 0, 0],
-  [1, -1, 9, 1],
-  [0, 9, -1, 5],
-  [0, 1, 5, -1]
+  [ 0 , 1 ,inf,inf],
+  [ 1 , 0 , 9 , 1 ],
+  [inf, 9 , 0 , 5 ],
+  [inf, 1 , 5 , 0 ]
 ]
-))
-    
+)
+
+"""
+Le graphe testé ci-dessus est :
+
+  [1] ---1km--- [2] ---9km--- [3] ---5km--- [0]
+                 |                           |
+                 |____________1km____________|
+"""
+#print("\n\n\n\n")
+#os.system("pause")
